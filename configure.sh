b08ddced7b9e35d25b66f573959759b7f8102560
Mike Taczak <mike.taczak@positronics.io>
2018-04-18 18:16:09 +0000

    Dont need this

--- configure.sh
+++ configure.sh
@@ -5,8 +5,3 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle; \
 git submodule update --init
 git submodule sync --recursive
 
-if type npm > /dev/null; then
-    npm install -g eslint
-else
-    echo "NPM is not install. Please install to enable eslint."
-fi
