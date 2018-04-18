89220edaf1866f7897f17fce62765e23ca653ebf
blakeatkins <blake.atkins@gmail.com>
2018-04-18 14:13:19 -0400

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -4,9 +4,3 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle; \
     https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 git submodule update --init
 git submodule sync --recursive
-
-if type npm > /dev/null; then
-    npm install -g eslint
-else
-    echo "NPM is not install. Please install to enable eslint."
-fi
